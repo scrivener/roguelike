78fcc37a96e26250dd48c89ddbdc9b3a15471fe7
Adam Lerner <eletari@gmail.com>
2014-12-23 11:50:54 -0500

    Added Monster script and attacking monsters.

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Monster : MonoBehaviour {
+
+	int health;
+	public int maxHealth;
+
+	// Use this for initialization
+	void Start () {
+		health = maxHealth;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public int getHealth() {
+		return health;
+	}
+	public void damage(int damage) {
+		health -= damage;
+	}
+}
--- Assets/Scripts/RoguelikeCharacterController.cs
+++ Assets/Scripts/RoguelikeCharacterController.cs
@@ -3,25 +3,41 @@ using System.Collections;
 
 public class RoguelikeCharacterController : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (Input.GetButtonDown ("Up")) {
-			moveCommand(new Vector3(0, 1, 0));
-		} else if (Input.GetButtonDown ("Down")) {
-			moveCommand(new Vector3(0, -1, 0));
-		} else if (Input.GetButtonDown ("Right")) {
-			moveCommand(new Vector3(1, 0, 0));
-		} else if (Input.GetButtonDown ("Left")) {
-			moveCommand(new Vector3(-1, 0, 0));
+    public int damage = 12;
+
+    // Use this for initialization
+    void Start () {
+    
+    }
+    
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetButtonDown ("Up")) {
+            moveCommand(Vector2.up);
+        } else if (Input.GetButtonDown ("Down")) {
+            moveCommand(-Vector2.up);
+        } else if (Input.GetButtonDown ("Right")) {
+            moveCommand(Vector2.right);
+        } else if (Input.GetButtonDown ("Left")) {
+            moveCommand(-Vector2.right);
+        }
+    }
+
+    void moveCommand(Vector3 where) {
+        RaycastHit2D hit = Physics2D.Raycast (transform.position, where, 1f, 1 << LayerMask.NameToLayer ("Enemy"));
+        if (!hit) {
+            transform.position += where;
+        } else {
+            resolveHit(hit);
+        }
+    }
+
+    void resolveHit(RaycastHit2D hit) {
+		Monster m = hit.collider.GetComponentInParent<Monster>();
+		if (m != null) {
+			m.damage (damage);
 		}
+		Debug.Log (m.getHealth());
 	}
 
-	void moveCommand(Vector3 where) {
-		transform.position += where;
-	}
 }
