7d9975ff269c59b6d2e82ef284db1e503cc1941e
Adam Lerner <eletari@gmail.com>
2014-12-23 12:10:03 -0500

    Monsters die when they run out of hit points. Particles for hits too.

--- Assets/Scripts/Monster.cs
+++ Assets/Scripts/Monster.cs
@@ -6,10 +6,13 @@ public class Monster : MonoBehaviour {
 	int health;
 	public int maxHealth;
 
+    ParticleSystem damagedParticle;
+
 	// Use this for initialization
 	void Start () {
 		health = maxHealth;
-	}
+        damagedParticle = GetComponentInChildren<ParticleSystem>();
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -21,5 +24,15 @@ public class Monster : MonoBehaviour {
 	}
 	public void damage(int damage) {
 		health -= damage;
+        if (damagedParticle != null) {
+            damagedParticle.Play();
+        }
+        if (health <= 0) {
+            die();
+        }
+
 	}
+    void die() {
+        Destroy(gameObject);
+    }
 }
