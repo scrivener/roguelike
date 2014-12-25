aef6acc5896d0b259e4de748d7effe42bd3ee4de
credeiki <rsrusk@gmail.com>
2014-12-25 12:53:24 -0500

    changed default monster placement to non-grid

--- Assets/Scripts/PlaceMonsters.cs
+++ Assets/Scripts/PlaceMonsters.cs
@@ -6,11 +6,18 @@ public Transform monster;
 	// Use this for initialization
 	// make a grid of monsters
 	void Start () {
-		for (int y = 0; y < 5; y++) {
-						for (int x = 0; x < 5; x++) {
-								Instantiate (monster, new Vector2 (x, y), Quaternion.identity);
+		int[] xpositions = {7, 12, -3, 14, 16, 0};
+		int[] ypositions = {4, 3, 1, 0, 0, 0};
+		int i = 0;
+		foreach (int x in xpositions) {
+
+		//initialize a grid of monsters with lines below
+		//for (int y = 0; y < 5; y++) {
+		//				for (int x = 0; x < 5; x++) {
+			Instantiate (monster, new Vector2 (x, ypositions[i]), Quaternion.identity);
+			i++;
 						}
-				}
+
 	}
 	
 	// Update is called once per frame
