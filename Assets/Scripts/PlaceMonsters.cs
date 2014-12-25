51cebb74b9a03030ea13719705e260d7710498c8
credeiki <rsrusk@gmail.com>
2014-12-24 22:40:16 -0500

    added monster placement

---
+++ Assets/Scripts/PlaceMonsters.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceMonsters : MonoBehaviour {
+public Transform monster;
+	// Use this for initialization
+	// make a grid of monsters
+	void Start () {
+		for (int y = 0; y < 5; y++) {
+						for (int x = 0; x < 5; x++) {
+								Instantiate (monster, new Vector2 (x, y), Quaternion.identity);
+						}
+				}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
