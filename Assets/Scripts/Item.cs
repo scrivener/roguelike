8011e23b855f407e63cffea560a557797809e3e4
Adam Lerner <adam.r.lerner@gmail.com>
2015-03-07 13:17:45 -0800

    Moved Inventory into scripts dir, added random stats to items.

--- Assets/Scripts/Item.cs
+++ Assets/Scripts/Item.cs
@@ -3,17 +3,21 @@ using System.Collections;
 
 public class Item : MonoBehaviour {
 
+	public int offense;
+	public int defense;
+
 	// Use this for initialization
 	void Start () {
-	
+		if (Random.value < 0.5) {
+			offense = Random.Range (1, 5);
+			defense = 0;
+		} else {
+			defense = Random.Range (1, 5);
+			offense = 0;
+		}
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
     public override string ToString() {
-        return "Item";
+		return string.Format("{0}/{1} Item", offense, defense);
     }
 }
